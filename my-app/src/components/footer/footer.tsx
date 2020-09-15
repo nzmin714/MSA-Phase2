97b8b325526b7515b01ee6871e540151fb7ec2f0
nzmin714 <64776125+nzmin714@users.noreply.github.com>
2020-09-15 20:12:08 +1200

    homepage

---
+++ my-app/src/components/footer/footer.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Footer = () => 
+    <div className="footer">&copy; All rights reserved Minhan Cho 2020.</div>
+
+export default Footer;
---
+++ my-app/src/components/header/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../logo.svg';
+
+const Header = () => 
+    <div className="nav-bar">
+        <img src={logo} className="header-logo" alt="logo" />
+        <p>Zonana</p>
+    </div>
+    
+
+export default Header;
